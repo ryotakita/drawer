24e7d30d2fb82f2a439fd2cec0478978dc220b62
ryota-kita <ryota-kita@kke.co.jp>
2020-01-29 13:26:21 +0900

    developGUItool(but, image don't print in GUI)

--- drawer.py
+++ drawer.py
@@ -4,43 +4,76 @@ import csv
 import math
 import svgwrite
 import random
+# -*- coding: utf8 -*-
+import os, tkinter, tkinter.filedialog, tkinter.messagebox
 
-img = np.full((3000, 3000, 3), 128, dtype=np.uint8)
+# ファイル選択ダイアログの表示
+root = tkinter.Tk()
+root.attributes("-topmost", True)
+root.title("SvgViewer")
+root.geometry("300x80")
 
-path_csv = input("ファイル名を入力してください。")
 
-f = open(path_csv, 'rt')
-dataReader = csv.reader(f)
+file = tkinter.StringVar()
+file.set("test.csv")
+def enterFile():
+    fTyp = [("","*")]
+    iDir = os.path.abspath(os.path.dirname(__file__))
+    fileName = tkinter.filedialog.askopenfilename(filetypes = fTyp,initialdir = iDir)
+    file.set(fileName)
 
-path_svg = path_csv.strip("csv") + "svg"
-path_png = path_csv.strip("csv") + "png"
-dwg = svgwrite.Drawing( path_svg,(3000,3000))
+button = tkinter.Button(root, text='パス指定',command = enterFile)
+button.pack()
 
-for row in dataReader:
-    if(len(row) == 4):
-        cv2.line(img, (int(round(float(row[0])))+500, int(round(float(row[1])))+500), (int(round(float(row[2])))+500, int(round(float(row[3])))+500), (0, 0, 0), thickness=1, lineType=cv2.LINE_4)
-        dwg.add( dwg.line((int(round(float(row[0])))+500,int(round(float(row[1])))+500),(int(round(float(row[2])))+500,int(round(float(row[3])))+500),  stroke=svgwrite.rgb(10, 10, 16, '%')))
-    elif(len(row) == 2):
-        x = int(round(float(row[0]))+500)
-        y = int(round(float(row[1]))+500)
-        dwg.add( dwg.circle((x,y), 1) )
-    else:
-        points = []
-        for num in range(len(row)):
-            if(num % 2 == 0) :
-                x = int(round(float(row[num]))+500)
-                y = int(round(float(row[num+1]))+500)
-                points.append([x,y])
-        r = random.randint(0,150)
-        g = random.randint(0,150)
-        b = random.randint(0,150)
-        print(points)
-        if(len(points) != 0):
-            dwg.add( dwg.polygon(points=points, fill=svgwrite.rgb(r,g,b,'%'), stroke="black") )
+def createSVG():
+    img = np.full((3000, 3000, 3), 128, dtype=np.uint8)
 
-cv2.imwrite(path_png, img)
-#dwg.add( dwg.polygon( points = points))
-dwg.save()
+    path_csv = file.get()
+    f = open(path_csv, 'rt')
+    datareader = csv.reader(f)
 
-dummy = input("処理が終了しました。出力ファイルは" + path_svg + "と" + path_png + "です。")
+    path_svg = path_csv.strip("csv") + "svg"
+    path_png = path_csv.strip("csv") + "png"
+    dwg = svgwrite.Drawing( path_svg,(3000,3000))
+
+    for row in datareader:
+        if(len(row) == 4):
+            dwg.add( dwg.line((int(round(float(row[0])))+500,int(round(float(row[1])))+500),( int(round(float(row[2])))+500,  int(round(float(row[3])))+500),  stroke=svgwrite.rgb(10, 10, 16, '%')))
+        elif(len(row) == 2):
+            x = int(round(float(row[0]))+500)
+            y = int(round(float(row[1]))+500)
+            dwg.add( dwg.circle((x,y), 1) )
+        else:
+            points = []
+            for num in range(len(row)):
+                if(num % 2 == 0) :
+                    x = int(round(float(row[num]))+500)
+                    y = int(round(float(row[num+1]))+500)
+                    points.append([x,y])
+            r = random.randint(0,150)
+            g = random.randint(0,150)
+            b = random.randint(0,150)
+            print(points)
+            if(len(points) != 0):
+                dwg.add( dwg.polygon(points=points, fill=svgwrite.rgb(r,g,b,'%'), stroke="black") )
+
+    #dwg.add( dwg.polygon( points = points))
+    dwg.save()
+
+    tkinter.messagebox.showinfo("処理結果","処理が終了しました。")
+
+button2 = tkinter.Button(root, text='実行',command = createSVG)
+button2.pack()
+
+Static1 = tkinter.Label(textvariable=file)
+Static1.pack()
+# 処理ファイル名の出力
+
+#tkinter.messagebox.showinfo('○×プログラム',file)
+
+root.mainloop()
+
+
+
+
 
